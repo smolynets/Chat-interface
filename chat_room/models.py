9db29d02c8be80b51568a36c07c392d88a5bd2d5
Oleh Smolynets <smolynets@gmail.com>
2019-06-12 14:00:01 +0300

    feat: added Room model

--- chat_room/models.py
+++ chat_room/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+"""
+Models for chat_room app.
+"""
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+class Room(models.Model):
+    """
+    Set Room model.
+    """
+
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = _("Room")
+        verbose_name_plural = _("Rooms")
+
+    def __str__(self):
+        return self.name
