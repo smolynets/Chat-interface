781b033c3af4ade77e4c1d04236830be9087e86b
Oleh Smolynets <smolynets@gmail.com>
2019-06-12 14:09:40 +0300

    fix: fix inheritance in message migrations

--- chat_room/migrations/0002_create_message.py
+++ chat_room/migrations/0002_create_message.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('chat_room', '0001_creare_room'),
+        ('chat_room', '0001_create_room'),
     ]
 
     operations = [
