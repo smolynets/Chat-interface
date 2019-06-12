7061af2ddfdf27bff1738835ee3bca492eb3c077
Oleh Smolynets <smolynets@gmail.com>
2019-06-12 17:56:10 +0300

    fix: added fields to Message model

--- chat_room/models.py
+++ chat_room/models.py
@@ -45,10 +45,14 @@ class Message(models.Model):
     Set message model.
     """
 
+    text = models.CharField(max_length=255, default="")
     room = models.ForeignKey(
         verbose_name=_("Message"), to="chat_room.Room",
         on_delete=models.CASCADE
     )
+    author = models.ForeignKey(verbose_name=_("author"), to="chat_room.User",
+                               null=True, related_name='author',
+                               on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = _("Message")
