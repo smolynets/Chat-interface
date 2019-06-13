156f49da8277460538ac29387838e6bb0868a015
Oleh Smolynets <smolynets@gmail.com>
2019-06-13 16:47:12 +0300

    feat: added models to admin

--- chat_room/admin.py
+++ chat_room/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Message, Room, User
+
+admin.site.register(Message)
+admin.site.register(Room)
+admin.site.register(User)
