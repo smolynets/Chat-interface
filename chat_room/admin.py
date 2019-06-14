315623c5354d4478bc8a67b072a9d61e5a8aa568
Oleh Smolynets <smolynets@gmail.com>
2019-06-14 16:06:45 +0300

    fix: last_message field of User is read_only in admin site

--- chat_room/admin.py
+++ chat_room/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Message, Room, User
 
+
+class UserAdmin(admin.ModelAdmin):
+    readonly_fields = ('last_message',)
+
+
 admin.site.register(Message)
 admin.site.register(Room)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
